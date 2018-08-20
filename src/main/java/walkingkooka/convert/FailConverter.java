7b3ea229df68bf7c1d5cfa931240c892140d88f7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-08-20 18:53:35 +1000

    A Converter that always fails for the given source and target.

---
+++ src/main/java/walkingkooka/convert/FailConverter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.convert;
+
+import java.util.Objects;
+
+/**
+ * A {@link Converter} that matches source of the given type and target type requests and ALWAYS fails.
+ */
+final class FailConverter<S, T> extends FixedTypeConverter2<S, T>{
+
+    static <S, T> FailConverter<S, T> with(final Class<S> source, final Class<T> target) {
+        Objects.requireNonNull(source, "source");
+        Objects.requireNonNull(target, "target");
+
+        return new FailConverter<>(source, target);
+    }
+
+    private FailConverter(final Class<S> source, final Class<T> target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    @Override
+    Class<S> sourceType() {
+        return this.source;
+    }
+
+    private final Class<S> source;
+
+    @Override
+    Class<T> targetType() {
+        return this.target;
+    }
+
+    private final Class<T> target;
+
+    @Override
+    T convert2(final S value) {
+        return this.failConversion(value);
+    }
+}
---
+++ src/test/java/walkingkooka/convert/FailConverterTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.convert;
+
+import org.junit.Test;
+import walkingkooka.Cast;
+
+import static org.junit.Assert.assertEquals;
+
+public final class FailConverterTest extends ConverterTestCase<FailConverter<String, Integer>>{
+
+    @Test(expected = NullPointerException.class)
+    public void testWithNullSourceTypeFails() {
+        FailConverter.with(null, Integer.class);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWithNullTargetTypeFails() {
+        FailConverter.with(String.class, null);
+    }
+
+    @Test
+    public void testConvertFails() {
+        this.convertFails("*FAILS*", Integer.class);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("String->Integer", this.createConverter().toString());
+    }
+
+    @Override
+    protected FailConverter<String, Integer> createConverter() {
+        return FailConverter.with(String.class, Integer.class);
+    }
+
+    @Override
+    protected Class<FailConverter<String, Integer>> type() {
+        return Cast.to(FailConverter.class);
+    }
+}
