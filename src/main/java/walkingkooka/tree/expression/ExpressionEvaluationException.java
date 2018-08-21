d7f0b2d4b202ced4c743a02b5936843a883558ff
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-08-21 17:22:15 +1000

    Fixed ExpressionEvaluationException changed to ExpressionException.

--- src/main/java/walkingkooka/tree/expression/ExpressionEvaluationException.java
+++ src/main/java/walkingkooka/tree/expression/ExpressionEvaluationException.java
@@ -18,9 +18,7 @@
 
 package walkingkooka.tree.expression;
 
-import walkingkooka.SystemException;
-
-public class ExpressionEvaluationException extends SystemException {
+public class ExpressionEvaluationException extends ExpressionException {
 
     private static final long serialVersionUID = 1;
 
