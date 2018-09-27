38e78fa1d0955dac0b25a23bb8d5663ff53f84d8
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-09-27 19:28:27 +1000

    Introduced VisitorPrettyPrinter and reworked a few visitors that pretty printed.
    - Also fixed two broken tests in SpreadsheetParsersTest (discovered after adding pretty printer).

--- src/test/java/walkingkooka/tree/search/SearchPrettySearchNodeVisitor.java
+++ src/test/java/walkingkooka/tree/search/SearchPrettySearchNodeVisitor.java
@@ -18,13 +18,12 @@
 
 package walkingkooka.tree.search;
 
-import walkingkooka.io.printer.IndentingPrinter;
 import walkingkooka.io.printer.IndentingPrinters;
 import walkingkooka.io.printer.Printers;
 import walkingkooka.text.Indentation;
 import walkingkooka.text.LineEnding;
-import walkingkooka.tree.Node;
 import walkingkooka.tree.visit.Visiting;
+import walkingkooka.tree.visit.VisitorPrettyPrinter;
 
 /**
  * Takes a {@link SearchNode} and pretty prints the nodes, making
@@ -33,132 +32,115 @@ final class SearchPrettySearchNodeVisitor extends SearchNodeVisitor {
 
     static String toString(final SearchNode node) {
         final StringBuilder b = new StringBuilder();
-        final IndentingPrinter printer = IndentingPrinters.printer(Printers.stringBuilder(b, LineEnding.NL));
-        new SearchPrettySearchNodeVisitor(printer, Indentation.with("  ")).accept(node);
+
+        new SearchPrettySearchNodeVisitor(VisitorPrettyPrinter.with(
+                IndentingPrinters.printer(Printers.stringBuilder(b, LineEnding.NL)),
+                Indentation.with("  "),
+                SearchPrettySearchNodeVisitor::tokenName)).accept(node);
         return b.toString();
     }
 
-    private SearchPrettySearchNodeVisitor(final IndentingPrinter printer, final Indentation indentation) {
+    private static String tokenName(final SearchNode token) {
+        final String typeName = token.getClass().getSimpleName();
+        return typeName.substring("Search".length(), typeName.length() - "ParserToken".length());
+    }
+
+    private SearchPrettySearchNodeVisitor(final VisitorPrettyPrinter<SearchNode> printer) {
         this.printer = printer;
-        this.indentation = indentation;
     }
 
     @Override
     protected Visiting startVisit(final SearchIgnoredNode node) {
-        this.enter(node);
+        this.printer.enter(node);
         return super.startVisit(node);
     }
 
     @Override
     protected void endVisit(final SearchIgnoredNode node) {
-        this.exit(node);
+        this.printer.exit(node);
         super.endVisit(node);
     }
-    
+
     @Override
     protected Visiting startVisit(final SearchMetaNode node) {
-        this.enter(node);
+        this.printer.enter(node);
         return super.startVisit(node);
     }
 
     @Override
     protected void endVisit(final SearchMetaNode node) {
-        this.exit(node);
+        this.printer.exit(node);
         super.endVisit(node);
     }
 
     @Override
     protected Visiting startVisit(final SearchSelectNode node) {
-        this.enter(node);
+        this.printer.enter(node);
         return super.startVisit(node);
     }
 
     @Override
     protected void endVisit(final SearchSelectNode node) {
-        this.exit(node);
+        this.printer.exit(node);
         super.endVisit(node);
     }
 
     @Override
     protected Visiting startVisit(final SearchSequenceNode node) {
-        this.enter(node);
+        this.printer.enter(node);
         return super.startVisit(node);
     }
 
     @Override
     protected void endVisit(final SearchSequenceNode node) {
-        this.exit(node);
+        this.printer.exit(node);
         super.endVisit(node);
     }
 
     @Override
     protected void visit(final SearchBigDecimalNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchBigIntegerNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchDoubleNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchLocalDateNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchLocalDateTimeNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchLocalTimeNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchLongNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
     @Override
     protected void visit(final SearchTextNode node) {
-        this.leaf(node);
+        this.printer.leaf(node);
     }
 
-    private void enter(final SearchNode node) {
-        this.printer.print(this.typeName(node));
-        this.printer.print(this.printer.lineEnding());
-        this.printer.indent(this.indentation);
-    }
+    private final VisitorPrettyPrinter<SearchNode> printer;
 
-    private void exit(final SearchNode node) {
-        this.printer.outdent();
+    @Override
+    public String toString() {
+        return this.printer.toString();
     }
-
-    private void leaf(final SearchNode node) {
-        this.printer.print(this.typeName(node) + "=" + node);
-        this.printer.print(this.printer.lineEnding());
-    }
-
-    private String typeName(final SearchNode node) {
-        String typeName = node.getClass().getSimpleName();
-        final String nodeName = Node.class.getSimpleName();
-        if(typeName.endsWith(nodeName)) {
-            typeName = typeName.substring(0, typeName.length() - nodeName.length());
-        }
-        final String search = "Search";
-        if(typeName.startsWith(search)){
-            typeName = typeName.substring(search.length());
-        }
-        return typeName;
-    }
-
-    private final IndentingPrinter printer;
-    private final Indentation indentation;
 }
---
+++ src/test/java/walkingkooka/tree/visit/VisitorPrettyPrinterTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.tree.visit;
+
+import org.junit.Test;
+import walkingkooka.Cast;
+import walkingkooka.io.printer.IndentingPrinter;
+import walkingkooka.io.printer.IndentingPrinters;
+import walkingkooka.io.printer.Printers;
+import walkingkooka.test.PublicClassTestCase;
+import walkingkooka.text.Indentation;
+import walkingkooka.text.LineEnding;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+public final class VisitorPrettyPrinterTest extends PublicClassTestCase<VisitorPrettyPrinter<Object>> {
+
+    @Test(expected = NullPointerException.class)
+    public void testWithNullPrinterFails() {
+        VisitorPrettyPrinter.with(null, this.indentation(), this.typeName());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWithNullIdentationFails() {
+        VisitorPrettyPrinter.with(this.printer(), null, this.typeName());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWithNullTypeNameFails() {
+        VisitorPrettyPrinter.with(this.printer(), this.indentation(), null);
+    }
+
+    @Test
+    public void testTree() {
+        final StringBuilder b = new StringBuilder();
+        final VisitorPrettyPrinter<Object> printer = this.prettyPrinter(b);
+
+        printer.enter("1");
+
+        printer.enter("2");
+        printer.leaf("3");
+        printer.exit("2");
+
+        printer.enter("4");
+        printer.leaf("5");
+        printer.exit("4");
+
+        printer.exit("1");
+
+        assertEquals("1\n  2\n    3=3\n  4\n    5=5\n", b.toString());
+    }
+
+    @Test
+    public void testToString() {
+        final StringBuilder b = new StringBuilder();
+        b.append("ABC");
+        assertEquals(b.toString(), this.prettyPrinter(b).toString());
+    }
+
+    private VisitorPrettyPrinter<Object> prettyPrinter(final StringBuilder b) {
+        return VisitorPrettyPrinter.with(
+                IndentingPrinters.printer(Printers.stringBuilder(b, LineEnding.NL)),
+                this.indentation(),
+                this.typeName());
+    }
+
+    private IndentingPrinter printer() {
+        return IndentingPrinters.fake();
+    }
+
+    private Indentation indentation() {
+        return Indentation.with("  ");
+    }
+
+    private final Function<Object, String> typeName() {
+        return (o -> o.toString());
+    }
+
+    @Override
+    protected Class<VisitorPrettyPrinter<Object>> type() {
+        return Cast.to(VisitorPrettyPrinter.class);
+    }
+}
