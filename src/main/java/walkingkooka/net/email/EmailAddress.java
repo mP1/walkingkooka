9f596fe82e7ae5a9cb96a380260bcdad4a9a4966
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-10-15 15:50:45 +1100

    EmailAddress.tryParse extracted from EmailAddress.with

--- src/main/java/walkingkooka/net/email/EmailAddress.java
+++ src/main/java/walkingkooka/net/email/EmailAddress.java
@@ -20,14 +20,11 @@ package walkingkooka.net.email;
 
 import walkingkooka.Value;
 import walkingkooka.net.HostAddress;
-import walkingkooka.predicate.character.CharPredicate;
-import walkingkooka.predicate.character.CharPredicateBuilder;
-import walkingkooka.predicate.character.CharPredicates;
 import walkingkooka.test.HashCodeEqualsDefined;
 import walkingkooka.text.CharSequences;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * A {@link Value} that holds a valid email address. Getters are available to get the components. Note that email address characters are validated
@@ -38,93 +35,17 @@ import java.util.Objects;
 final public class EmailAddress implements Value<String>, HashCodeEqualsDefined, Serializable {
 
     /**
+     * Tries to parse the email or returns an {@link Optional#empty()}.
+     */
+    public static Optional<EmailAddress> tryParse(final String email) {
+        return EmailAddressParserTryParse.tryParse(email);
+    }
+
+    /**
      * Creates and validates the given email.
      */
     public static EmailAddress with(final String email) {
-        Objects.requireNonNull(email, "email");
-
-        if (email.length() >= EmailAddress.MAX_EMAIL_LENGTH) {
-            EmailAddress.fail(EmailAddress.EMAIL_TOO_LONG);
-        }
-
-        final int length = email.length();
-        boolean quoted = false;
-        int charactersSinceDot = 0;
-        int userNameCharacterCount = 0;
-        char previous = 0;
-
-        HostAddress hostAddress = null;
-        String user = null;
-
-        for (int i = 0; i < length; i++) {
-            final char c = email.charAt(i);
-            final char wasPrevious = previous;
-            previous = c;
-
-            // found opening quote ?
-            if ((false == quoted) && ('"' == c)) {
-                quoted = true;
-                continue;
-            }
-            if (('.' != c) && EmailAddress.USERNAME_CHARACTERS.test(c)) {
-                charactersSinceDot++;
-                continue;
-            }
-
-            if (quoted) {
-                // support escaped quoted double quotes
-                if ('\\' == wasPrevious) {
-                    if ('"' == c) {
-                        continue;
-                    }
-                    if ('\\' == c) {
-                        previous = 0;
-                        continue;
-                    }
-                }
-                if (EmailAddress.QUOTABLE_USERNAME_CHARACTERS.test(c)) {
-                    charactersSinceDot++;
-                    continue;
-                }
-                // found closing quote
-                if (c == '\"') {
-                    quoted = false;
-                    continue;
-                }
-                continue;
-            }
-            // only match dot if previous was not a dot.
-            if (('.' == c) && (0 != charactersSinceDot)) {
-                userNameCharacterCount += charactersSinceDot;
-                charactersSinceDot = 0;
-                continue;
-            }
-            if ((false == quoted) && (c == '@')) {
-                if ('.' == wasPrevious) {
-                    EmailAddress.fail(EmailAddress.invalidCharacter(email, i - 1));
-                }
-                userNameCharacterCount += charactersSinceDot;
-                if (0 == userNameCharacterCount) {
-                    EmailAddress.fail(EmailAddress.missingUser(email));
-                }
-                // if @ is the last character complain because host is missing.
-                if ((length - 1) == i) {
-                    EmailAddress.fail(EmailAddress.missingHost(email));
-                }
-                if (userNameCharacterCount > EmailAddress.MAX_LOCAL_LENGTH) {
-                    EmailAddress.fail(EmailAddress.userNameTooLong(userNameCharacterCount));
-                }
-                user = email.substring(0, i);
-                hostAddress = HostAddress.withEmail(email, i + 1);
-                break;
-            }
-
-            EmailAddress.fail(EmailAddress.invalidCharacter(email, i));
-        }
-        if (null == hostAddress) {
-            EmailAddress.fail(EmailAddress.missingHost(email));
-        }
-        return new EmailAddress(email, user, hostAddress);
+        return EmailAddressParserWith.parseOrFail(email);
     }
 
     /**
@@ -133,19 +54,14 @@ final public class EmailAddress implements Value<String>, HashCodeEqualsDefined,
     final static int MAX_EMAIL_LENGTH = 255;
 
     /**
-     * Email is too long
+     * The message when an email is too long.
      */
     final static String EMAIL_TOO_LONG = "Email too long";
 
-    private final static CharPredicate USERNAME_CHARACTERS = CharPredicateBuilder.create()//
-            .range('A', 'Z')//
-            .range('a', 'z')//
-            .range('0', '9')//
-            .any("!#$%&'*+-/=?^_`{|}~")//
-            .any('.')//
-            .build();
-
-    private final static CharPredicate QUOTABLE_USERNAME_CHARACTERS = CharPredicates.any(" <>[]:;@\\");
+    /**
+     * The maximum number of characters that can appear in as a user name in an email.
+     */
+    final static int MAX_LOCAL_LENGTH = 64;
 
     /**
      * Message when an email is missing a user.
@@ -153,11 +69,6 @@ final public class EmailAddress implements Value<String>, HashCodeEqualsDefined,
     static String missingUser(final String address) {
         return "Missing user=" + CharSequences.quote(address);
     }
-
-    /**
-     * The maximum number of characters that can appear in as a user name in an email.
-     */
-    final static int MAX_LOCAL_LENGTH = 64;
 
     /**
      * Message when a user name is too long.
@@ -181,10 +92,10 @@ final public class EmailAddress implements Value<String>, HashCodeEqualsDefined,
     }
 
     /**
-     * Used to report a failure with the email address.
+     * Unconditionally creates a new {@link EmailAddress}
      */
-    static void fail(final String message) {
-        throw new IllegalArgumentException(message);
+    static EmailAddress with0(final String address, final String user, final HostAddress host) {
+        return new EmailAddress(address, user, host);
     }
 
     /**
