7584b114b4fdb711814c54c937349b18813194f0
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-10-15 20:27:04 +1100

    EmailAddressPredicate.

---
+++ src/test/java/walkingkooka/predicate/EmailAddressPredicateTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.predicate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class EmailAddressPredicateTest extends PredicateTestCase<EmailAddressPredicate, String> {
+
+    @Test
+    public void testValid() {
+        this.testTrue("user@example.com");
+    }
+
+    @Test
+    public void testIncomplete() {
+        this.testFalse("user@");
+    }
+
+    @Test
+    public void testInvalid() {
+        this.testFalse("123");
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("EmailAddress", this.createPredicate().toString());
+    }
+
+    @Override
+    protected EmailAddressPredicate createPredicate() {
+        return EmailAddressPredicate.INSTANCE;
+    }
+
+    @Override
+    protected Class<EmailAddressPredicate> type() {
+        return EmailAddressPredicate.class;
+    }
+}
