6140cfa49f77de4007d27cea0481b9a5ccbd80b6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-11-19 21:01:35 +1100

    HttpResponse interface and testcase.

---
+++ src/main/java/walkingkooka/net/http/HttpResponse.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.net.http;
+
+import java.util.Map;
+
+/**
+ * Defines a HTTP response.
+ */
+public interface HttpResponse {
+
+    /**
+     * Sets the code and message.
+     */
+    void setStatus(final int code, final String message);
+
+    /**
+     * Returns a {@link Map} view of all request headers.
+     */
+    <T> void addHeader(final HttpHeaderName name, final T value);
+}
