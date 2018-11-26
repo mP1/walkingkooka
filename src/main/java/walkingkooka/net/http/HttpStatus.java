8a5061bb6e042d71c91d92c7ff1ccb81356d7be2
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-11-26 19:43:37 +1100

    HttpStatus and HttpStatusCode.

--- src/main/java/walkingkooka/net/http/HttpStatus.java
+++ src/main/java/walkingkooka/net/http/HttpStatus.java
@@ -19,401 +19,55 @@
 package walkingkooka.net.http;
 
 import walkingkooka.Cast;
-import walkingkooka.collect.map.Maps;
+import walkingkooka.Value;
 import walkingkooka.test.HashCodeEqualsDefined;
 import walkingkooka.text.Whitespace;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+import java.util.Objects;
 
 /**
- * Holds a HTTP response and message.
+ * Holds a HTTP response status code and message.
  */
-public final class HttpStatus implements HashCodeEqualsDefined {
-
-    /**
-     * Factory that creates a {@link HttpStatus}. This factory is called exclusively by all constants in this class.
-     */
-    private static HttpStatus registerConstant(final int value, final String message) {
-        final HttpStatus statusCode = new HttpStatus(value, message);
-        CONSTANTS.put(value, statusCode);
-        return statusCode;
-    }
-
-    /**
-     * Holds all constants keyed by their code. This must appear before the constants so the map is initialized and not null.
-     */
-    private static Map<Integer, HttpStatus> CONSTANTS = Maps.hash();
-
-    /**
-     * Continue={@link HttpServletResponse#SC_CONTINUE}
-     */
-    public final static HttpStatus CONTINUE = registerConstant(HttpServletResponse.SC_CONTINUE, "Continue");
-
-    /**
-     * Switching protocols={@link HttpServletResponse#SC_SWITCHING_PROTOCOLS}
-     */
-    public final static HttpStatus SWITCHING_PROTOCOLS = registerConstant(
-            HttpServletResponse.SC_SWITCHING_PROTOCOLS,
-            "Switching protocols");
-
-    // Success
-
-    /**
-     * OK={@link HttpServletResponse#SC_OK}
-     */
-    public final static HttpStatus OK = registerConstant(HttpServletResponse.SC_OK, "OK");
-
-    /**
-     * Created={@link HttpServletResponse#SC_CREATED}
-     */
-    public final static HttpStatus CREATED = registerConstant(HttpServletResponse.SC_CREATED, "Created");
-
-    /**
-     * Accepted={@link HttpServletResponse#SC_ACCEPTED}
-     */
-    public final static HttpStatus ACCEPTED = registerConstant(HttpServletResponse.SC_ACCEPTED, "Accepted");
-
-    /**
-     * Non Authoritative information={@link HttpServletResponse#SC_NON_AUTHORITATIVE_INFORMATION}
-     */
-    public final static HttpStatus NON_AUTHORITATIVE_INFORMATION = registerConstant(
-            HttpServletResponse.SC_NON_AUTHORITATIVE_INFORMATION,
-            "Non authoritative information");
-
-    /**
-     * No content={@link HttpServletResponse#SC_NO_CONTENT}
-     */
-    public final static HttpStatus NO_CONTENT = registerConstant(HttpServletResponse.SC_NO_CONTENT,
-            "No content");
-
-    /**
-     * Reset content={@link HttpServletResponse#SC_RESET_CONTENT}
-     */
-    public final static HttpStatus RESET_CONTENT = registerConstant(HttpServletResponse.SC_RESET_CONTENT,
-            "Reset content");
-
-    /**
-     * Partial content={@link HttpServletResponse#SC_PARTIAL_CONTENT}
-     */
-    public final static HttpStatus PARTIAL_CONTENT = registerConstant(HttpServletResponse.SC_PARTIAL_CONTENT,
-            "Partial content");
-
-    // Redirect
-
-    /**
-     * Multiple choices={@link HttpServletResponse#SC_MULTIPLE_CHOICES}
-     */
-    public final static HttpStatus MULTIPLE_CHOICES = registerConstant(
-            HttpServletResponse.SC_MULTIPLE_CHOICES,
-            "Multiple choices");
-
-    /**
-     * Moved permanently={@link HttpServletResponse#SC_MOVED_PERMANENTLY}
-     */
-    public final static HttpStatus MOVED_PERMANENTLY = registerConstant(
-            HttpServletResponse.SC_MOVED_PERMANENTLY,
-            "Moved permanently");
-
-    /**
-     * Found={@link HttpServletResponse#SC_MOVED_TEMPORARILY}
-     */
-    public final static HttpStatus MOVED_TEMPORARILY = registerConstant(
-            HttpServletResponse.SC_MOVED_TEMPORARILY,
-            "Moved temporarily");
-
-    /**
-     * Found={@link HttpServletResponse#SC_FOUND}
-     */
-    public final static HttpStatus FOUND = registerConstant(HttpServletResponse.SC_FOUND, "Found");
-
-    /**
-     * See other={@link HttpServletResponse#SC_SEE_OTHER}
-     */
-    public final static HttpStatus SEE_OTHER = registerConstant(HttpServletResponse.SC_SEE_OTHER,
-            "See other");
-
-    /**
-     * Not Modified={@link HttpServletResponse#SC_NOT_MODIFIED}
-     */
-    public final static HttpStatus NOT_MODIFIED = registerConstant(HttpServletResponse.SC_NOT_MODIFIED,
-            "Not modified");
-
-    /**
-     * User Proxy={@link HttpServletResponse#SC_USE_PROXY}
-     */
-    public final static HttpStatus USE_PROXY = registerConstant(HttpServletResponse.SC_USE_PROXY,
-            "User proxy");
-
-    /**
-     * Temporary redirect={@link HttpServletResponse#SC_TEMPORARY_REDIRECT}
-     */
-    public final static HttpStatus TEMPORARY_REDIRECT = registerConstant(
-            HttpServletResponse.SC_TEMPORARY_REDIRECT,
-            "Temporary redirect");
-
-    // Client Error
-
-    /**
-     * Bad request={@link HttpServletResponse#SC_BAD_REQUEST}
-     */
-    public final static HttpStatus BAD_REQUEST = registerConstant(HttpServletResponse.SC_BAD_REQUEST,
-            "Bad request");
-
-    /**
-     * Unauthorized={@link HttpServletResponse#SC_UNAUTHORIZED}
-     */
-    public final static HttpStatus UNAUTHORIZED = registerConstant(HttpServletResponse.SC_UNAUTHORIZED,
-            "Unauthorized");
-
-    /**
-     * Payment required={@link HttpServletResponse#SC_PAYMENT_REQUIRED}
-     */
-    public final static HttpStatus PAYMENT_REQUIRED = registerConstant(
-            HttpServletResponse.SC_PAYMENT_REQUIRED,
-            "Payment required");
-
-    /**
-     * Forbidden={@link HttpServletResponse#SC_FORBIDDEN}
-     */
-    public final static HttpStatus FORBIDDEN = registerConstant(HttpServletResponse.SC_FORBIDDEN,
-            "Forbidden");
-
-    /**
-     * Not found={@link HttpServletResponse#SC_NOT_FOUND}
-     */
-    public final static HttpStatus NOT_FOUND = registerConstant(HttpServletResponse.SC_NOT_FOUND,
-            "Not found");
-
-    /**
-     * Method not allowed={@link HttpServletResponse#SC_METHOD_NOT_ALLOWED}
-     */
-    public final static HttpStatus METHOD_NOT_ALLOWED = registerConstant(
-            HttpServletResponse.SC_METHOD_NOT_ALLOWED,
-            "Method not allowed");
-
-    /**
-     * Not acceptable={@link HttpServletResponse#SC_NOT_ACCEPTABLE}
-     */
-    public final static HttpStatus NOT_ACCEPTABLE = registerConstant(HttpServletResponse.SC_NOT_ACCEPTABLE,
-            "Not acceptable");
-
-    /**
-     * Request timeout={@link HttpServletResponse#SC_REQUEST_TIMEOUT}
-     */
-    public final static HttpStatus REQUEST_TIMEOUT = registerConstant(HttpServletResponse.SC_REQUEST_TIMEOUT,
-            "Request timeout");
-
-    /**
-     * Conflict={@link HttpServletResponse#SC_CONFLICT}
-     */
-    public final static HttpStatus CONFLICT = registerConstant(HttpServletResponse.SC_CONFLICT, "Conflict");
-
-    /**
-     * Gone={@link HttpServletResponse#SC_GONE}
-     */
-    public final static HttpStatus GONE = registerConstant(HttpServletResponse.SC_GONE, "Gone");
-
-    /**
-     * Length required={@link HttpServletResponse#SC_LENGTH_REQUIRED}
-     */
-    public final static HttpStatus LENGTH_REQUIRED = registerConstant(HttpServletResponse.SC_LENGTH_REQUIRED,
-            "Length required");
-
-    /**
-     * Precondition failed={@link HttpServletResponse#SC_PRECONDITION_FAILED}
-     */
-    public final static HttpStatus PRECONDITION_FAILED = registerConstant(
-            HttpServletResponse.SC_PRECONDITION_FAILED,
-            "Precondition failed");
-
-    /**
-     * Proxy Authentication required={@link HttpServletResponse#SC_PROXY_AUTHENTICATION_REQUIRED}
-     */
-    public final static HttpStatus PROXY_AUTHENTICATION_REQUIRED = registerConstant(
-            HttpServletResponse.SC_PROXY_AUTHENTICATION_REQUIRED,
-            "Proxy Authentication required");
-
-    /**
-     * Request Entity Too Large={@link HttpServletResponse#SC_REQUEST_ENTITY_TOO_LARGE}
-     */
-    public final static HttpStatus REQUEST_ENTITY_TOO_LARGE = registerConstant(
-            HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE,
-            "Request entity too large");
-
-    /**
-     * Request-URI Too Long={@link HttpServletResponse#SC_REQUEST_URI_TOO_LONG}
-     */
-    public final static HttpStatus REQUEST_URI_TOO_LONG = registerConstant(
-            HttpServletResponse.SC_REQUEST_URI_TOO_LONG,
-            "Request-URI Too Long");
-
-    /**
-     * Unsupported Media Type={@link HttpServletResponse#SC_UNSUPPORTED_MEDIA_TYPE}
-     */
-    public final static HttpStatus UNSUPPORTED_MEDIA_TYPE = registerConstant(
-            HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE,
-            "Unsupported Media Type");
-
-    /**
-     * Requested Range Not Satisfiable={@link HttpServletResponse#SC_REQUESTED_RANGE_NOT_SATISFIABLE}
-     */
-    public final static HttpStatus REQUESTED_RANGE_NOT_SATISFIABLE = registerConstant(
-            HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE, "Requested Range Not Satisfiable");
-
-    /**
-     * Expectation Failed={@link HttpServletResponse#SC_EXPECTATION_FAILED}
-     */
-    public final static HttpStatus EXPECTATION_FAILED = registerConstant(
-            HttpServletResponse.SC_EXPECTATION_FAILED,
-            "Expectation Failed");
-
-    // Server Error
-
-    /**
-     * Internal Server Error={@link HttpServletResponse#SC_INTERNAL_SERVER_ERROR}
-     */
-    public final static HttpStatus INTERNAL_SERVER_ERROR = registerConstant(
-            HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-            "Internal Server Error");
-
-    /**
-     * Not Implemented={@link HttpServletResponse#SC_NOT_IMPLEMENTED}
-     */
-    public final static HttpStatus NOT_IMPLEMENTED = registerConstant(HttpServletResponse.SC_NOT_IMPLEMENTED,
-            "Not implemented");
-
-    /**
-     * Bad Gateway={@link HttpServletResponse#SC_BAD_GATEWAY}
-     */
-    public final static HttpStatus BAD_GATEWAY = registerConstant(HttpServletResponse.SC_BAD_GATEWAY,
-            "Bad Gateway");
-
-    /**
-     * Service Unavailable={@link HttpServletResponse#SC_SERVICE_UNAVAILABLE}
-     */
-    public final static HttpStatus SERVICE_UNAVAILABLE = registerConstant(
-            HttpServletResponse.SC_SERVICE_UNAVAILABLE,
-            "Service Unavailable");
-
-    /**
-     * Gateway Timeout={@link HttpServletResponse#SC_GATEWAY_TIMEOUT}
-     */
-    public final static HttpStatus GATEWAY_TIMEOUT = registerConstant(HttpServletResponse.SC_GATEWAY_TIMEOUT,
-            "Gateway Timeout");
-
-    /**
-     * HTTP Version Not Supported={@link HttpServletResponse#SC_HTTP_VERSION_NOT_SUPPORTED}
-     */
-    public final static HttpStatus HTTP_VERSION_NOT_SUPPORTED = registerConstant(
-            HttpServletResponse.SC_HTTP_VERSION_NOT_SUPPORTED,
-            "HTTP Version Not Supported");
-
-    /**
-     * Lookup that retrieves the {@link HttpStatus} which includes a standard default message with the given code, returning null if unknown.
-     */
-    public static HttpStatus withCode(final int code) {
-        return CONSTANTS.get(code);
-    }
+public final class HttpStatus implements HashCodeEqualsDefined, Value<HttpStatusCode> {
 
     /**
      * Factory that creates a {@link HttpStatus}
      */
-    public static HttpStatus with(final int value, final String message) {
-        checkCode(value);
-        checkMessage(message);
-
+    static HttpStatus with(final HttpStatusCode value, final String message) {
         return new HttpStatus(value, message);
-    }
-
-    private static void checkCode(final int value) {
-        if (value < 0) {
-            throw new IllegalArgumentException("Code " + value + " must be >= 0");
-        }
-    }
-
-    private static void checkMessage(final String message) {
-        Whitespace.failIfNullOrWhitespace(message, "message");
     }
 
     /**
      * Private constructor use static factory.
      */
-    private HttpStatus(final int value, final String message) {
+    private HttpStatus(final HttpStatusCode value, final String message) {
         super();
         this.value = value;
         this.message = message;
     }
 
-    /**
-     * Tests if code is informational (100-199)
-     */
-    public boolean isInformational() {
-        return this.isRange(100, 199);
-    }
-
-    /**
-     * Tests if code is successful (200-299)
-     */
-    public boolean isSuccessful() {
-        return this.isRange(200, 299);
-    }
-
-    /**
-     * Tests if code is a redirect
-     *
-     * <pre>
-     * HTTP status codes 3xx
-     * In the HTTP protocol used by the World Wide Web, a redirect is a response with a status code beginning with 3 that causes a browser to display a different page. The different codes describe the reason for the redirect, which allows for the correct subsequent action (such as changing links in the case of code 301, a permanent change of address).
-     * The HTTP standard defines several status codes for redirection:
-     * 300 multiple choices (e.g. offer different languages)
-     * 301 moved permanently
-     * 302 found (originally temporary redirect, but now commonly used to specify redirection for unspecified reason)
-     * 303 see other (e.g. for results of cgi-scripts)
-     * 307 temporary redirect
-     * </pre>
-     * <p>
-     * <a href="http://en.wikipedia.org/wiki/HTTP_redirect"></a>}
-     */
-    public boolean isRedirect() {
-        final int value = this.value;
-        return value == MULTIPLE_CHOICES.value || //
-                value == MOVED_PERMANENTLY.value || //
-                value == FOUND.value || //
-                value == SEE_OTHER.value || //
-                value == TEMPORARY_REDIRECT.value;
-    }
-
-    /**
-     * Tests if code is a client error (400-499)
-     */
-    public boolean isClientError() {
-        return this.isRange(400, 499);
-    }
-
-    /**
-     * Tests if code is a server error (500-599)
-     */
-    public boolean isServerError() {
-        return this.isRange(500, 599);
-    }
-
-    /**
-     * Helper that tests if the actual value is between the lower and upper range(inclusive).
-     */
-    private boolean isRange(final int lower, final int upper) {
-        final int value = this.value;
-        return value >= lower && value <= upper;
-    }
 
     /**
      * Getter that returns the code
      */
-    public int value() {
+    @Override
+    public HttpStatusCode value() {
         return this.value;
     }
 
-    private final int value;
+    /**
+     * Would be setter that returns a {@link HttpStatus} with the new message. Together with this method any of the constants may be used to set a
+     * message inheriting the status code.
+     */
+    public HttpStatus setCode(final HttpStatusCode code) {
+        Objects.requireNonNull(code, "code");
+
+        return this.value.equals(code) ?
+                this :
+                this.replace(code, this.message);
+    }
+
+    private final HttpStatusCode value;
 
     /**
      * Getter that returns the message accompanying the code
@@ -431,36 +85,48 @@ public final class HttpStatus implements HashCodeEqualsDefined {
 
         return this.message.equals(message) ?
                 this :
-                new HttpStatus(this.value, message);
+                this.replace(this.value, message);
     }
 
     private final String message;
+
+    private static void checkMessage(final String message) {
+        Whitespace.failIfNullOrWhitespace(message, "message");
+    }
+
+    // replace........................................................................
+
+    private HttpStatus replace(final HttpStatusCode value, final String message) {
+        return new HttpStatus(value, message);
+    }
 
     // Object........................................................................
 
     @Override
     public int hashCode() {
-        return this.value;
+        return Objects.hash(this.value, this.message);
     }
 
     @Override
     public boolean equals(final Object other) {
         return this == other ||
-                other instanceof HttpStatus && this.equals0(Cast.to(other));
+                other instanceof HttpStatus &&
+                        this.equals0(Cast.to(other));
     }
 
     /**
      * Note only the value and not the message is included in equality tests.
      */
     private boolean equals0(final HttpStatus other) {
-        return this.value == other.value;
+        return this.value == other.value &&
+                this.message.equals(other.message);
     }
 
     /**
-     * Dumps the status message followed by the code
+     * Dumps the code and message in the format of a response.
      */
     @Override
     public String toString() {
-        return this.value + "=" + this.message;
+        return this.value.code() + " " + this.message;
     }
 }
--- src/test/java/walkingkooka/net/http/HttpStatusTest.java
+++ src/test/java/walkingkooka/net/http/HttpStatusTest.java
@@ -20,210 +20,86 @@ package walkingkooka.net.http;
 
 import org.junit.Test;
 import walkingkooka.test.PublicClassTestCase;
-import walkingkooka.type.FieldAttributes;
-import walkingkooka.type.MemberVisibility;
-
-import java.lang.reflect.Field;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
 final public class HttpStatusTest extends PublicClassTestCase<HttpStatus> {
 
     // constants
 
-    private final static int CODE = 200;
-    private final static String MESSAGE = "message";
-
-    // tests
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testWithInvalidCodeFails() {
-        HttpStatus.with(-1, HttpStatusTest.MESSAGE);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testWithNullMessageFails() {
-        HttpStatus.with(HttpStatusTest.CODE, null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testWithEmptyMessageFails() {
-        HttpStatus.with(HttpStatusTest.CODE, "");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testWithWhitespaceOnlyMessageFails() {
-        HttpStatus.with(HttpStatusTest.CODE, "   ");
-    }
+    private final static HttpStatusCode CODE = HttpStatusCode.OK;
+    private final static String MESSAGE = "OK";
 
     @Test
     public void testWith() {
-        final HttpStatus code = HttpStatus.with(HttpStatusTest.CODE, HttpStatusTest.MESSAGE);
-        assertEquals("value", HttpStatusTest.CODE, code.value());
-        assertSame("message", HttpStatusTest.MESSAGE, code.message());
+        this.check(this.status(), CODE, MESSAGE);
     }
 
+    // setCode ................................................................
+
     @Test(expected = NullPointerException.class)
-    public void testSetNullMessageFails() {
-        HttpStatus.with(HttpStatusTest.CODE, HttpStatusTest.MESSAGE).setMessage(null);
+    public void testSetCodeNullFails() {
+        status().setCode(null);
+    }
+
+    @Test
+    public void testSetCodeSame() {
+        final HttpStatus status = this.status();
+        assertSame(status, status.setCode(CODE));
+    }
+
+    @Test
+    public void testSetCodeDifferent() {
+        final HttpStatus status = this.status();
+        final HttpStatusCode code = HttpStatusCode.BAD_REQUEST;
+        final HttpStatus different = status.setCode(code);
+        assertNotSame(status, different);
+        this.check(different, code, MESSAGE);
+    }
+
+    // setMessage ................................................................
+
+    @Test(expected = NullPointerException.class)
+    public void testSetMessageNullFails() {
+        status().setMessage(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSetEmptyMessageFails() {
-        HttpStatus.with(HttpStatusTest.CODE, HttpStatusTest.MESSAGE).setMessage("");
+    public void testSetMessageEmptyFails() {
+        status().setMessage("");
     }
 
     @Test
-    public void testSetSameMessage() {
-        final HttpStatus code = HttpStatus.with(HttpStatusTest.CODE, HttpStatusTest.MESSAGE);
-        assertSame(code, code.setMessage(HttpStatusTest.MESSAGE));
+    public void testSetMessageSame() {
+        final HttpStatus status = this.status();
+        assertSame(status, status.setMessage(MESSAGE));
     }
 
     @Test
-    public void testSetDifferentMessage() {
-        final HttpStatus code = HttpStatus.with(HttpStatusTest.CODE, "different");
-        final HttpStatus updated = code.setMessage(HttpStatusTest.MESSAGE);
-        assertNotSame(code, updated);
-        assertEquals("value", HttpStatusTest.CODE, updated.value());
-        assertSame("message", HttpStatusTest.MESSAGE, updated.message());
+    public void testSetMessageDifferent() {
+        final HttpStatus status = this.status();
+        final String message = "different";
+        final HttpStatus different = status.setMessage(message);
+        assertNotSame(status, different);
+        this.check(different, CODE, message);
     }
 
-    @Test
-    public void testUnknownCodeReturnsNull() {
-        assertNull(HttpStatus.withCode(999));
+    // helpers ..................................
+
+    private HttpStatus status() {
+        return HttpStatus.with(CODE, MESSAGE);
     }
 
-    @Test
-    public void testKnownCode() throws Exception {
-        for (final Field constant : HttpStatus.class.getDeclaredFields()) {
-            if (false == constant.getType().equals(HttpStatus.class)) {
-                continue;
-            }
-            assertTrue(constant + " is NOT public", MemberVisibility.PUBLIC.is(constant));
-            assertTrue(constant + " is NOT static", FieldAttributes.STATIC.is(constant));
-
-            final HttpStatus httpStatusCode = (HttpStatus) constant.get(null);
-            final int code = httpStatusCode.value();
-            if (code == 302) {
-                continue;
-            }
-            assertSame(httpStatusCode, HttpStatus.withCode(httpStatusCode.value()));
-        }
-    }
-
-    @Test
-    public void testContinue() {
-        final HttpStatus code = HttpStatus.CONTINUE;
-        assertTrue(code + " should be a informational", code.isInformational());
-        assertFalse(code + " should not be a successful", code.isSuccessful());
-        assertFalse(code + " should not be a redirect", code.isRedirect());
-        assertFalse(code + " should not be a client error", code.isClientError());
-        assertFalse(code + " should not be server error", code.isServerError());
-    }
-
-    @Test
-    public void testOk() {
-        final HttpStatus code = HttpStatus.OK;
-        assertFalse(code + " should not be informational", code.isInformational());
-        assertTrue(code + " should be a successful", code.isSuccessful());
-        assertFalse(code + " should not be redirect", code.isRedirect());
-        assertFalse(code + " should not be a client error", code.isClientError());
-        assertFalse(code + " should not be server error", code.isServerError());
-    }
-
-    /**
-     * <pre>
-     * In the HTTP protocol used by the World Wide Web, a redirect is a response with a status code beginning with 3 that causes a browser to display a different page. The different codes describe the reason for the redirect, which allows for the correct subsequent action (such as changing links in the case of code 301, a permanent change of address).
-     * The HTTP standard defines several status codes for redirection:
-     * 300 multiple choices (e.g. offer different languages)
-     * 301 moved permanently
-     * 302 found (originally temporary redirect, but now commonly used to specify redirection for unspecified reason)
-     * 303 see other (e.g. for results of cgi-scripts)
-     * 307 temporary redirect
-     * All of these status codes require that the URL of the redirect target be given in the Location: header of the HTTP response. The 300 multiple choices will usually list all choices in the body of the message and show the default choice in the Location: header.
-     * </pre>
-     *
-     * <a href="http://en.wikipedia.org/wiki/HTTP_redirect"></a>
-     */
-    @Test
-    public void testMultipleChoicesIsRedirect() {
-        this.checkRedirect(HttpStatus.MULTIPLE_CHOICES);
-    }
-
-    @Test
-    public void testMovePermanentlyIsRedirect() {
-        this.checkRedirect(HttpStatus.MOVED_PERMANENTLY);
-    }
-
-    @Test
-    public void testFoundIsRedirect() {
-        this.checkRedirect(HttpStatus.FOUND);
-    }
-
-    @Test
-    public void testSeeOtherIsRedirect() {
-        this.checkRedirect(HttpStatus.SEE_OTHER);
-    }
-
-    @Test
-    public void testTemporaryRedirectIsRedirect() {
-        this.checkRedirect(HttpStatus.TEMPORARY_REDIRECT);
-    }
-
-    private void checkRedirect(final HttpStatus status) {
-        assertTrue(status + " should be a redirect", status.isRedirect());
-    }
-
-    /**
-     * <pre>
-     * (Status codes 304 not modified and 305 use proxy are not redirects).
-     * </pre>
-     *
-     * <a href="http://en.wikipedia.org/wiki/HTTP_redirect"></a>}
-     */
-    @Test
-    public void testNotModifiedNotRedirect() {
-        this.checkNotRedirect(HttpStatus.NOT_MODIFIED);
-    }
-
-    @Test
-    public void testUseProxyNotRedirect() {
-        this.checkNotRedirect(HttpStatus.USE_PROXY);
-    }
-
-    private void checkNotRedirect(final HttpStatus status) {
-        assertFalse(status + " should NOT be a redirect", status.isRedirect());
-    }
-
-    @Test
-    public void testNotFound() {
-        final HttpStatus code = HttpStatus.NOT_FOUND;
-        assertFalse(code + " should not be a informational", code.isInformational());
-        assertFalse(code + " should not be a successful", code.isSuccessful());
-        assertFalse(code + " should not be a redirect", code.isRedirect());
-        assertTrue(code + " should not be a client error", code.isClientError());
-        assertFalse(code + " should not be a server error", code.isServerError());
-    }
-
-    @Test
-    public void testInternalServerError() {
-        final HttpStatus code = HttpStatus.INTERNAL_SERVER_ERROR;
-        assertFalse(code + " should not be a informational", code.isInformational());
-        assertFalse(code + " should not be a successful", code.isSuccessful());
-        assertFalse(code + " should not be a redirect", code.isRedirect());
-        assertFalse(code + " should not be a client error", code.isClientError());
-        assertTrue(code + " should be a server error", code.isServerError());
+    private void check(final HttpStatus status, final HttpStatusCode code, final String message) {
+        assertEquals("value", code, status.value());
+        assertEquals("message", message, status.message());
     }
 
     @Test
     public void testToString() {
-        assertEquals(HttpStatusTest.CODE + "=" + HttpStatusTest.MESSAGE,
-                HttpStatus.with(HttpStatusTest.CODE, HttpStatusTest.MESSAGE).toString());
+        assertEquals("200 OK", this.status().toString());
     }
 
     @Override
