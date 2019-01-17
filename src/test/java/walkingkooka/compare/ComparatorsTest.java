6b587601a3794c57e6f645062cd48c4188f3c916
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-01-17 18:33:20 +1100

    ComparatorsTest consolidate all Comparators tests

--- src/test/java/walkingkooka/compare/ComparatorsTest.java
+++ src/test/java/walkingkooka/compare/ComparatorsTest.java
@@ -17,11 +17,56 @@
 
 package walkingkooka.compare;
 
+import org.junit.Test;
 import walkingkooka.test.PublicStaticHelperTestCase;
 
 import java.lang.reflect.Method;
 
+import static org.junit.Assert.assertEquals;
+
 final public class ComparatorsTest extends PublicStaticHelperTestCase<Comparators> {
+
+    @Test
+    public void testNormalizeIntZero() {
+        this.normalizeIntAndCheck(0, 0);
+    }
+
+    @Test
+    public void testNormalizeIntPositive() {
+        this.normalizeIntAndCheck(2, 1);
+    }
+
+    @Test
+    public void testNormalizeIntNegative() {
+        this.normalizeIntAndCheck(-3, -1);
+    }
+
+    private void normalizeIntAndCheck(final int value, final int expected) {
+        assertEquals("Normalize (int)" + value,
+                expected,
+                Comparables.normalize(value));
+    }
+
+    @Test
+    public void testNormalizeLongZero() {
+        this.normalizeLongAndCheck(0L, 0L);
+    }
+
+    @Test
+    public void testNormalizeLongPositive() {
+        this.normalizeLongAndCheck(2L, 1L);
+    }
+
+    @Test
+    public void testNormalizeLongNegative() {
+        this.normalizeLongAndCheck(-3L, -1L);
+    }
+
+    private void normalizeLongAndCheck(final long value, final long expected) {
+        assertEquals("Normalize (long)" + value,
+                expected,
+                Comparables.normalize(value));
+    }
 
     // helpers
 
