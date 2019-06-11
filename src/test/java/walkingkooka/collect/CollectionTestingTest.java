1ee72a30cf5d4bc21e321687008f4aeb494fc624
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-06-11 14:03:36 +1000

    CollectionTesting addFails, removeFails

---
+++ src/test/java/walkingkooka/collect/CollectionTestingTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package walkingkooka.collect;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+
+import java.util.Collections;
+import java.util.List;
+
+public final class CollectionTestingTest implements CollectionTesting<List<String>, String> {
+
+    private final static String ELEMENT1 = "a1";
+    private final static String ELEMENT2 = "b2";
+
+    @Test
+    public void testAddFails() {
+        this.addFails(Collections.emptyList(), "add!");
+    }
+
+    @Test
+    public void testIsEmptyEmpty() {
+        this.isEmptyAndCheck(Lists.empty(), true);
+    }
+
+    @Test
+    public void testIsEmptyNotEmpty() {
+        this.isEmptyAndCheck(this.createCollection(), false);
+    }
+
+    @Test
+    public void testSize() {
+        this.sizeAndCheck(this.createCollection(), 2);
+    }
+
+    @Test
+    public void testRemoveFails() {
+        this.removeFails(this.createCollection(), ELEMENT1);
+    }
+
+    @Override
+    public void testCheckToStringOverridden() {
+    }
+
+    @Override
+    public List<String> createCollection() {
+        return Lists.of(ELEMENT1, ELEMENT2);
+    }
+
+    @Override
+    public Class<List<String>> type() {
+        return Cast.to(List.class);
+    }
+}
