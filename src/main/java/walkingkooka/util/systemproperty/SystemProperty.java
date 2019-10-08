d222b787c9b997eee75f41ef490bcd2b140ad745
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-10-08 22:01:33 +1100

    SystemProperty minor improvements.
    - get: no longer update constants cache when new properties are requested.
    - toString: removed auto quote if necessary

--- src/main/java/walkingkooka/util/systemproperty/SystemProperty.java
+++ src/main/java/walkingkooka/util/systemproperty/SystemProperty.java
@@ -22,71 +22,71 @@ import walkingkooka.test.HashCodeEqualsDefined;
 import walkingkooka.text.CaseSensitivity;
 import walkingkooka.text.CharSequences;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.WeakHashMap;
 
 /**
- * Represents the key to a system property. A getter exists to retrieve the actual value.<br>
+ * Represents a system property with methods to interact with values.<br>
  * <a href="https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html"></a>
  */
 final public class SystemProperty implements Value<String>, HashCodeEqualsDefined {
 
     /**
-     * A cache of {@link SystemProperty}.
+     * A cache of {@link SystemProperty} constants.
      */
-    private final static Map<String, SystemProperty> PROPERTIES = new WeakHashMap<String, SystemProperty>();
+    private final static Map<String, SystemProperty> PROPERTIES = new HashMap<String, SystemProperty>();
 
-    public final static SystemProperty FILE_ENCODING = createAndPut("file.encoding");
+    public final static SystemProperty FILE_ENCODING = registerConstant("file.encoding");
 
-    public final static SystemProperty FILE_ENCODING_PKG = createAndPut("file.encoding.pkg");
+    public final static SystemProperty FILE_ENCODING_PKG = registerConstant("file.encoding.pkg");
 
-    public final static SystemProperty FILE_SEPARATOR = createAndPut("file.separator");
+    public final static SystemProperty FILE_SEPARATOR = registerConstant("file.separator");
 
-    public final static SystemProperty JAVA_CLASS_PATH = createAndPut("java.class.path");
+    public final static SystemProperty JAVA_CLASS_PATH = registerConstant("java.class.path");
 
-    public final static SystemProperty JAVA_CLASS_PATH_SEPARATOR = createAndPut("path.separator");
+    public final static SystemProperty JAVA_CLASS_PATH_SEPARATOR = registerConstant("path.separator");
 
-    public final static SystemProperty JAVA_CLASS_VERSION = createAndPut("java.class.version");
+    public final static SystemProperty JAVA_CLASS_VERSION = registerConstant("java.class.version");
 
-    public final static SystemProperty JAVA_COMPILER = createAndPut("java.compiler");
+    public final static SystemProperty JAVA_COMPILER = registerConstant("java.compiler");
 
-    public final static SystemProperty JAVA_HOME = createAndPut("java.home");
+    public final static SystemProperty JAVA_HOME = registerConstant("java.home");
 
-    public final static SystemProperty JAVA_IO_TMPDIR = createAndPut("java.io.tmpdir");
+    public final static SystemProperty JAVA_IO_TMPDIR = registerConstant("java.io.tmpdir");
 
-    public final static SystemProperty JAVA_VERSION = createAndPut("java.version");
+    public final static SystemProperty JAVA_VERSION = registerConstant("java.version");
 
-    public final static SystemProperty JAVA_VENDOR = createAndPut("java.vendor");
+    public final static SystemProperty JAVA_VENDOR = registerConstant("java.vendor");
 
-    public final static SystemProperty JAVA_VENDOR_URL = createAndPut("java.vendor.url");
+    public final static SystemProperty JAVA_VENDOR_URL = registerConstant("java.vendor.url");
 
-    public final static SystemProperty LINE_SEPARATOR = createAndPut("line.separator");
+    public final static SystemProperty LINE_SEPARATOR = registerConstant("line.separator");
 
-    public final static SystemProperty OS_NAME = createAndPut("os.name");
+    public final static SystemProperty OS_NAME = registerConstant("os.name");
 
-    public final static SystemProperty OS_ARCH = createAndPut("os.arch");
+    public final static SystemProperty OS_ARCH = registerConstant("os.arch");
 
-    public final static SystemProperty OS_VERSION = createAndPut("os.version");
+    public final static SystemProperty OS_VERSION = registerConstant("os.version");
 
-    public final static SystemProperty USER_DIR = createAndPut("user.dir");
+    public final static SystemProperty USER_DIR = registerConstant("user.dir");
 
-    public final static SystemProperty USER_HOME = createAndPut("user.home");
+    public final static SystemProperty USER_HOME = registerConstant("user.home");
 
-    public final static SystemProperty USER_LANGUAGE = createAndPut("user.language");
+    public final static SystemProperty USER_LANGUAGE = registerConstant("user.language");
 
-    public final static SystemProperty USER_NAME = createAndPut("user.name");
+    public final static SystemProperty USER_NAME = registerConstant("user.name");
 
-    public final static SystemProperty USER_REGION = createAndPut("user.region");
+    public final static SystemProperty USER_REGION = registerConstant("user.region");
 
-    public final static SystemProperty USER_TIMEZONE = createAndPut("user.timezone");
+    public final static SystemProperty USER_TIMEZONE = registerConstant("user.timezone");
 
     // our properties
 
     /**
      * {@see walkingkooka.text.CaseSensitivity#FILE_SYSTEM_CASE_SENSITIVITY}
      */
-    public final static SystemProperty FILE_SYSTEM_CASE_SENSITIVITY = createAndPut(CaseSensitivity.class.getName());
+    public final static SystemProperty FILE_SYSTEM_CASE_SENSITIVITY = registerConstant(CaseSensitivity.class.getName());
 
     /**
      * Retrieves a {@link SystemProperty} with the given name.
@@ -96,7 +96,7 @@ final public class SystemProperty implements Value<String>, HashCodeEqualsDefine
 
         SystemProperty property = SystemProperty.PROPERTIES.get(name);
         if (null == property) {
-            property = SystemProperty.createAndPut(name);
+            property = new SystemProperty(name);
         }
         return property;
     }
@@ -104,16 +104,17 @@ final public class SystemProperty implements Value<String>, HashCodeEqualsDefine
     /**
      * Creates then updates the cache and returns the new {@link SystemProperty}.
      */
-    synchronized private static SystemProperty createAndPut(final String name) {
+    synchronized private static SystemProperty registerConstant(final String name) {
         final SystemProperty property = new SystemProperty(name);
         SystemProperty.PROPERTIES.put(name, property);
         return property;
     }
 
     /**
-     * Private constructor
+     * Private constructor use {@link #get(String)}
      */
     private SystemProperty(final String name) {
+        super();
         this.name = name;
     }
 
@@ -174,7 +175,7 @@ final public class SystemProperty implements Value<String>, HashCodeEqualsDefine
         return System.clearProperty(this.name);
     }
 
-    // Object
+    // Object..........................................................................................................
 
     @Override
     public int hashCode() {
@@ -183,8 +184,8 @@ final public class SystemProperty implements Value<String>, HashCodeEqualsDefine
 
     @Override
     public boolean equals(final Object other) {
-        return (this == other) || ((other instanceof SystemProperty)
-                && this.equals0((SystemProperty) other));
+        return this == other ||
+                (other instanceof SystemProperty && this.equals0((SystemProperty) other));
     }
 
     private boolean equals0(final SystemProperty other) {
@@ -193,6 +194,6 @@ final public class SystemProperty implements Value<String>, HashCodeEqualsDefine
 
     @Override
     public String toString() {
-        return CharSequences.quoteIfNecessary(this.name).toString();
+        return this.name;
     }
 }
