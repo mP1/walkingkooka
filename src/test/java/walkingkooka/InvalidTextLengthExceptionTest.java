19eef559de2425c57cee6206dd843e666669f1c9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-19 22:50:17 +1100

    InvalidTextLengthException.label getter

--- src/test/java/walkingkooka/InvalidTextLengthExceptionTest.java
+++ src/test/java/walkingkooka/InvalidTextLengthExceptionTest.java
@@ -77,33 +77,52 @@ public final class InvalidTextLengthExceptionTest implements ThrowableTesting2<I
     @Test
     public void testWith() {
         final InvalidTextLengthException cause = this.create();
-        check(cause, TEXT, MIN, MAX);
+        check(cause,
+                LABEL,
+                TEXT,
+                MIN,
+                MAX);
     }
 
     @Test
     public void testWithEmptyText() {
         final InvalidTextLengthException cause = new InvalidTextLengthException(LABEL, "", 0, MAX);
-        check(cause, "", 0, MAX);
+        check(cause, LABEL,
+                "",
+                0,
+                MAX);
     }
 
     @Test
     public void testWith2() {
         final InvalidTextLengthException cause = new InvalidTextLengthException(LABEL, "abc", 0, 4);
-        check(cause, "abc", 0, 4);
+        check(cause,
+                LABEL,
+                "abc",
+                0,
+                4);
     }
 
     @Test
     public void testWithCause() {
         final Throwable cause = new Exception();
         final InvalidTextLengthException thrown = new InvalidTextLengthException(LABEL, "abc", 0, 4, cause);
-        check(thrown, "abc", 0, 4);
+        check(thrown,
+                LABEL,
+                "abc",
+                0,
+                4);
         this.checkCause(thrown, cause);
     }
 
     @Test
     public void testWithMinEqualsMax() {
         final InvalidTextLengthException cause = new InvalidTextLengthException(LABEL, "abcd", 4, 4);
-        check(cause, "abcd", 4, 4);
+        check(cause,
+                LABEL,
+                "abcd",
+                4,
+                4);
     }
 
     @Test
@@ -123,9 +142,11 @@ public final class InvalidTextLengthExceptionTest implements ThrowableTesting2<I
     }
 
     private void check(final InvalidTextLengthException exception,
+                       final String label,
                        final String text,
                        final int min,
                        final int max) {
+        assertEquals(label, exception.label(), "label");
         assertEquals(text, exception.text(), "text");
         assertEquals(min, exception.min(), min);
         assertEquals(max, exception.max(), max);
