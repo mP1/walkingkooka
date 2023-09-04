06b51bd9a5663a39a9247847628076632889a8da
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-09-05 07:28:59 +1000

    ToStringBuilderAppenderLongScalarTest test naming fix
    - No logic changes, just a method rename.

--- src/test/java/walkingkooka/ToStringBuilderAppenderLongScalarTest.java
+++ src/test/java/walkingkooka/ToStringBuilderAppenderLongScalarTest.java
@@ -42,7 +42,7 @@ public final class ToStringBuilderAppenderLongScalarTest extends ToStringBuilder
     }
 
     @Test
-    public void testLongPrimitiveexWholeNumberWithAtoFLetters() {
+    public void testLongPrimitiveHexWholeNumberWithAtoFLetters() {
         final ToStringBuilder b = this.builder();
         b.enable(ToStringBuilderOption.HEX_WHOLE_NUMBERS);
 
