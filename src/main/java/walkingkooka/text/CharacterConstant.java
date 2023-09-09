c9c6489b64a124acfadf4d236ddfb99485991aed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-09-09 10:49:18 +1000

    CharacterConstant parse/toSeparatedString

--- src/main/java/walkingkooka/text/CharacterConstant.java
+++ src/main/java/walkingkooka/text/CharacterConstant.java
@@ -17,6 +17,15 @@
 
 package walkingkooka.text;
 
+import walkingkooka.InvalidCharacterException;
+import walkingkooka.collect.list.Lists;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * Holds both a character and its {@link String} equivalent. This is ideal for constants that should
  * not be inlined and is intended to replace dual declaration of constants of for the character and
@@ -100,7 +109,86 @@ final public class CharacterConstant implements CharSequence {
         throw new StringIndexOutOfBoundsException(message);
     }
 
-    // HasHashCodeAndEqualsDefined
+    // parse............................................................................................................
+
+    /**
+     * Parses the given {@link String text} into csv delimited components, each component is then parsed.
+     * An empty input {@link String} will produce an empty {@link List}.
+     */
+    public <T> List<T> parse(final String text,
+                             final Function<String, T> component) {
+        Objects.requireNonNull(text, "text");
+
+        final List<T> parsed = Lists.array();
+
+        final int length = text.length();
+        int i = 0;
+        int start = 0;
+
+        while (i < length) {
+            final char c = text.charAt(i);
+            if (this.character == c) {
+                parse0(
+                        text,
+                        start,
+                        i,
+                        component,
+                        parsed
+                );
+
+                start = i + 1;
+            }
+            i++;
+        }
+
+        if (start != length) {
+            parse0(
+                    text,
+                    start,
+                    length,
+                    component,
+                    parsed
+            );
+        }
+
+        return Lists.immutable(parsed);
+    }
+
+    private static <T> void parse0(final String text,
+                                   final int start,
+                                   final int end,
+                                   final Function<String, T> component,
+                                   final List<T> parsed) {
+        try {
+            parsed.add(
+                    component.apply(text.substring(start, end))
+            );
+        } catch (final InvalidCharacterException cause) {
+            throw cause.setTextAndPosition(
+                    text,
+                    start + cause.position()
+            );
+        } catch (final RuntimeException cause) {
+            throw new IllegalArgumentException("Unable to parse " + CharSequences.quote(text) + ", " + cause.getMessage(), cause);
+        }
+    }
+
+    // toSeparatedString..................................................................................................
+
+    /**
+     * THe inverse of the {link #parse}, turns the given values back into a CSV String.
+     */
+    public <T> String toSeparatedString(final Collection<T> values,
+                                        final Function<T, String> component) {
+        Objects.requireNonNull(values, "values");
+        Objects.requireNonNull(component, "component");
+
+        return values.stream()
+                .map(component)
+                .collect(Collectors.joining(this.string));
+    }
+
+    // Object..........................................................................................................
 
     @Override
     public int hashCode() {
--- src/test/java/walkingkooka/text/CharacterConstantTest.java
+++ src/test/java/walkingkooka/text/CharacterConstantTest.java
@@ -18,13 +18,21 @@
 package walkingkooka.text;
 
 import org.junit.jupiter.api.Test;
+import walkingkooka.InvalidCharacterException;
+import walkingkooka.collect.list.Lists;
 import walkingkooka.reflect.ClassTesting2;
 import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.test.ParseStringTesting;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-final public class CharacterConstantTest implements ClassTesting2<CharacterConstant>,
+final public class CharacterConstantTest implements ParseStringTesting<List<Integer>>,
+        ClassTesting2<CharacterConstant>,
         CharSequenceTesting<CharacterConstant> {
 
     private final static char CHAR = 'a';
@@ -68,6 +76,166 @@ final public class CharacterConstantTest implements ClassTesting2<CharacterConst
         this.checkEquals(String.valueOf(c), constant.string(), "string");
     }
 
+    // parse............................................................................................................
+
+    @Override
+    public void testParseStringEmptyFails() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testParseEmpty() {
+        this.parseStringAndCheck(
+                "",
+                Lists.empty()
+        );
+    }
+
+    @Test
+    public void testParseFunctionFails() {
+        this.parseStringFails(
+                "123,abc",
+                new IllegalArgumentException("Unable to parse \"123,abc\", For input string: \"abc\"")
+        );
+    }
+
+    @Test
+    public void testParseFunctionThrowsInvalidCharacterExceptionFails() {
+        final InvalidCharacterException thrown = assertThrows(
+                InvalidCharacterException.class,
+                () -> CharacterConstant.with('.')
+                        .parse(
+                                "123,45*",
+                                (s) -> {
+                                    throw new InvalidCharacterException(s, 3);
+                                }
+                        )
+        );
+        this.checkEquals(
+                "Invalid character ',' at 3 in \"123,45*\"",
+                thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testParseFunctionThrowsInvalidCharacterExceptionFails2() {
+        final InvalidCharacterException thrown = assertThrows(
+                InvalidCharacterException.class,
+                () -> CharacterConstant.with(',')
+                        .parse(
+                                "123,45*",
+                                (s) -> {
+                                    if (s.equals("123")) {
+                                        return 123;
+                                    }
+                                    throw new InvalidCharacterException(s, 2);
+                                }
+                        )
+        );
+        this.checkEquals(
+                "Invalid character '*' at 6 in \"123,45*\"",
+                thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testParseOne() {
+        this.parseStringAndCheck(
+                "123",
+                Lists.of(
+                        123
+                )
+        );
+    }
+
+    @Test
+    public void testParseTwo() {
+        this.parseStringAndCheck(
+                "123,456",
+                Lists.of(
+                        123,
+                        456
+                )
+        );
+    }
+
+    @Test
+    public void testParseThree() {
+        this.parseStringAndCheck(
+                "123,456,789",
+                Lists.of(
+                        123,
+                        456,
+                        789
+                )
+        );
+    }
+
+    // toSeparatedString............................................................................................................
+
+    @Test
+    public void testToSeparatedStringEmpty() {
+        this.toSeparatedStringAndCheck(
+                Lists.empty(),
+                Function.identity(),
+                ""
+        );
+    }
+
+    @Test
+    public void testToSeparatedStringOne() {
+        this.toSeparatedStringAndCheck(
+                Lists.of("abc"),
+                s -> CharSequences.quote(s).toString(),
+                "\"abc\""
+        );
+    }
+
+    @Test
+    public void testToSeparatedStringSeveral() {
+        this.toSeparatedStringAndCheck(
+                Lists.of("abc", "def", "ghi"),
+                s -> CharSequences.quote(s).toString(),
+                "\"abc\",\"def\",\"ghi\""
+        );
+    }
+
+    private <T> void toSeparatedStringAndCheck(final Collection<T> values,
+                                               final Function<T, String> component,
+                                               final String expected) {
+        this.checkEquals(
+                expected,
+                CharacterConstant.with(',')
+                        .toSeparatedString(
+                                values,
+                                component
+                        )
+        );
+    }
+
+    // ParseStringTesting...............................................................................................
+
+    @Override
+    public List<Integer> parseString(final String text) {
+        return CharacterConstant.with(',')
+                .parse(
+                        text,
+                        Integer::parseInt
+                );
+    }
+
+    @Override
+    public Class<? extends RuntimeException> parseStringFailedExpected(Class<? extends RuntimeException> expected) {
+        return expected;
+    }
+
+    @Override
+    public RuntimeException parseStringFailedExpected(final RuntimeException expected) {
+        return expected;
+    }
+
+    // Object..........................................................................................................
+
     @Test
     public void testEqualsDifferent() {
         this.checkNotEquals(CharacterConstant.with((char) (CHAR
@@ -78,6 +246,8 @@ final public class CharacterConstantTest implements ClassTesting2<CharacterConst
     public void testToString() {
         this.toStringAndCheck(CharacterConstant.with('a'), "a");
     }
+
+    // CharSequence.....................................................................................................
 
     @Override
     public CharacterConstant createCharSequence() {
