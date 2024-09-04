8393e832791fc4e205a052ab1ff6b309340f16dd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-09-04 19:58:49 +1000

    ImmutableListTesting internal refactoring

--- src/main/java/walkingkooka/collect/list/ImmutableListTesting.java
+++ src/main/java/walkingkooka/collect/list/ImmutableListTesting.java
@@ -30,15 +30,15 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
     default void concatAndCheck(final ImmutableList<E> list,
                                 final E appended,
                                 final ImmutableList<E> expected) {
-        final ImmutableList<E> appendAndNew = list.concat(appended);
+        final ImmutableList<E> afterConcat = list.concat(appended);
 
         assertNotSame(
-                appendAndNew,
+                afterConcat,
                 list
         );
         this.checkEquals(
                 expected,
-                appendAndNew,
+                afterConcat,
                 () -> list + " concat " + appended
         );
 
@@ -46,7 +46,7 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
         toList.add(appended);
         this.checkEquals(
                 toList,
-                appendAndNew,
+                afterConcat,
                 () -> list + " concat " + appended
         );
     }
@@ -54,15 +54,15 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
     default void removeAtIndexAndCheck(final ImmutableList<E> list,
                                        final int removed,
                                        final ImmutableList<E> expected) {
-        final ImmutableList<E> removedAndNew = list.removeAtIndex(removed);
+        final ImmutableList<E> afterRemove = list.removeAtIndex(removed);
 
         assertNotSame(
-                removedAndNew,
+                afterRemove,
                 list
         );
         this.checkEquals(
                 expected,
-                removedAndNew,
+                afterRemove,
                 () -> list + " removeAtIndex " + removed
         );
 
@@ -70,7 +70,7 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
         toList.remove(removed);
         this.checkEquals(
                 toList,
-                removedAndNew,
+                afterRemove,
                 () -> list + " removeAtIndex " + removed
         );
     }
@@ -78,15 +78,15 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
     default void removeElementAndCheck(final ImmutableList<E> list,
                                        final E remove,
                                        final ImmutableList<E> expected) {
-        final ImmutableList<E> removed = list.removeElement(remove);
+        final ImmutableList<E> afterRemove = list.removeElement(remove);
 
         assertNotSame(
-                removed,
+                afterRemove,
                 list
         );
         this.checkEquals(
                 expected,
-                removed,
+                afterRemove,
                 () -> list + " removeElement " + remove
         );
 
@@ -94,8 +94,25 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
         toList.remove(remove);
         this.checkEquals(
                 toList,
-                removed,
+                afterRemove,
                 () -> list + " removeElement " + remove
+        );
+    }
+
+    default void replaceAndCheck(final ImmutableList<E> list,
+                                 final int index,
+                                 final E replace,
+                                 final ImmutableList<E> expected) {
+        final ImmutableList<E> afterReplace = list.replace(index, replace);
+
+        assertNotSame(
+                replace,
+                list
+        );
+        this.checkEquals(
+                expected,
+                afterReplace,
+                () -> list + " replaced " + index + ", " + replace
         );
     }
 
@@ -134,32 +151,15 @@ public interface ImmutableListTesting<L extends ImmutableList<E>, E> extends Lis
                               final int left,
                               final int right,
                               final ImmutableList<E> expected) {
-        final ImmutableList<E> swapped = list.swap(left, right);
+        final ImmutableList<E> afterSwap = list.swap(left, right);
         assertNotSame(
-                swapped,
+                afterSwap,
                 list
         );
         this.checkEquals(
                 expected,
-                swapped,
+                afterSwap,
                 () -> list + " swap " + left + ", " + right
-        );
-    }
-
-    default void replaceAndCheck(final ImmutableList<E> list,
-                                 final int index,
-                                 final E replace,
-                                 final ImmutableList<E> expected) {
-        final ImmutableList<E> replaced = list.replace(index, replace);
-
-        assertNotSame(
-                replace,
-                list
-        );
-        this.checkEquals(
-                expected,
-                replaced,
-                () -> list + " replaced " + index + ", " + replace
         );
     }
 
