29cb1d3b988a57cff182103053d6019fee511882
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-14 13:11:17 +1000

    TextPrinting
    - Closes https://github.com/mP1/walkingkooka/issues/3179
    - TextPrinting implements TextContext

---
+++ src/main/java/walkingkooka/text/TextPrinting.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.text;
+
+import walkingkooka.ToStringBuilder;
+import walkingkooka.UsesToStringBuilder;
+
+import java.util.Objects;
+
+/**
+ * An immutable {@link TextPrinting}.
+ */
+public final class TextPrinting implements TextContext,
+    UsesToStringBuilder {
+
+    public static TextPrinting with(final Indentation indentation,
+                                    LineEnding lineEnding) {
+        return new TextPrinting(
+            Objects.requireNonNull(indentation, "indentation"),
+            Objects.requireNonNull(lineEnding, "lineEnding")
+        );
+    }
+
+    private TextPrinting(final Indentation indentation,
+                         LineEnding lineEnding) {
+        super();
+
+        this.indentation = indentation;
+        this.lineEnding = lineEnding;
+    }
+
+    // HasIndentation...................................................................................................
+
+    @Override
+    public Indentation indentation() {
+        return this.indentation;
+    }
+
+    public TextPrinting setIndentation(final Indentation indentation) {
+        return this.indentation.equals(indentation) ?
+            this :
+            new TextPrinting(
+                Objects.requireNonNull(indentation, "indentation"),
+                this.lineEnding
+            );
+    }
+
+    private Indentation indentation;
+
+    // HasLineEnding....................................................................................................
+
+    @Override
+    public LineEnding lineEnding() {
+        return this.lineEnding;
+    }
+
+    public TextPrinting setLineEnding(final LineEnding lineEnding) {
+        return this.lineEnding.equals(lineEnding) ?
+            this :
+            new TextPrinting(
+                this.indentation,
+                Objects.requireNonNull(lineEnding, "lineEnding")
+            );
+    }
+
+    private LineEnding lineEnding;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            this.indentation,
+            this.lineEnding
+        );
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+            (other instanceof TextPrinting &&
+                this.equals0((TextPrinting) other));
+    }
+
+    private boolean equals0(final TextPrinting other) {
+        return this.indentation.equals(other.indentation) &&
+            this.lineEnding.equals(other.lineEnding);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.buildFrom(this);
+    }
+
+    // UsesToStringBuilder..............................................................................................
+
+    @Override
+    public void buildToString(final ToStringBuilder b) {
+        b.label("indentation");
+        b.value(
+            CharSequences.escape(this.indentation)
+        );
+
+        b.label("lineEnding");
+        b.value(
+            CharSequences.escape(this.lineEnding)
+        );
+    }
+}
---
+++ src/test/java/walkingkooka/text/TextPrintingTest.java
@@ -0,0 +1,242 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.text;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.ToStringTesting;
+import walkingkooka.reflect.ClassTesting;
+import walkingkooka.reflect.JavaVisibility;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class TextPrintingTest implements TextContextTesting,
+    HashCodeEqualsDefinedTesting2<TextPrinting>,
+    ToStringTesting<TextPrinting>,
+    ClassTesting<TextPrinting> {
+
+    private final static Indentation INDENTATION = Indentation.SPACES2;
+
+    private final static Indentation DIFFERENT_INDENTATION = Indentation.SPACES4;
+
+    private final static LineEnding LINE_ENDING = LineEnding.NL;
+
+    private final static LineEnding DIFFERENT_LINE_ENDING = LineEnding.CR;
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullIndentationFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> TextPrinting.with(
+                null,
+                LINE_ENDING
+            )
+        );
+    }
+
+    @Test
+    public void testWithNullLineEndingFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> TextPrinting.with(
+                INDENTATION,
+                null
+            )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final TextPrinting textPrinting = TextPrinting.with(
+            INDENTATION,
+            LINE_ENDING
+        );
+
+        this.indentationAndCheck(
+            textPrinting,
+            INDENTATION
+        );
+
+        this.lineEndingAndCheck(
+            textPrinting,
+            LINE_ENDING
+        );
+    }
+
+    // setIndentation...................................................................................................
+
+    @Test
+    public void testSetIndentationWithNullFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> this.createObject()
+                .setIndentation(null)
+        );
+    }
+
+    @Test
+    public void testSetIndentationSame() {
+        final TextPrinting textPrinting = this.createObject();
+
+        assertSame(
+            textPrinting,
+            textPrinting.setIndentation(INDENTATION)
+        );
+    }
+
+    @Test
+    public void testSetIndentationDifferent() {
+        final TextPrinting textPrinting = this.createObject();
+
+        final TextPrinting different = textPrinting.setIndentation(DIFFERENT_INDENTATION);
+
+        assertNotSame(
+            textPrinting,
+            different
+        );
+
+        this.indentationAndCheck(
+            different,
+            DIFFERENT_INDENTATION
+        );
+
+        this.lineEndingAndCheck(
+            different,
+            LINE_ENDING
+        );
+
+        this.indentationAndCheck(
+            textPrinting,
+            INDENTATION
+        );
+
+        this.lineEndingAndCheck(
+            textPrinting,
+            LINE_ENDING
+        );
+    }
+
+    // setLineEnding....................................................................................................
+
+    @Test
+    public void testSetLineEndingWithNullFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> this.createObject()
+                .setLineEnding(null)
+        );
+    }
+
+    @Test
+    public void testSetLineEndingSame() {
+        final TextPrinting textPrinting = this.createObject();
+
+        assertSame(
+            textPrinting,
+            textPrinting.setLineEnding(LINE_ENDING)
+        );
+    }
+
+    @Test
+    public void testSetLineEndingDifferent() {
+        final TextPrinting textPrinting = this.createObject();
+
+        final TextPrinting different = textPrinting.setLineEnding(DIFFERENT_LINE_ENDING);
+
+        assertNotSame(
+            textPrinting,
+            different
+        );
+
+        this.indentationAndCheck(
+            different,
+            INDENTATION
+        );
+
+        this.lineEndingAndCheck(
+            different,
+            DIFFERENT_LINE_ENDING
+        );
+
+        this.indentationAndCheck(
+            textPrinting,
+            INDENTATION
+        );
+
+        this.lineEndingAndCheck(
+            textPrinting,
+            LINE_ENDING
+        );
+    }
+
+    // hashCode/equals..................................................................................................
+
+    @Test
+    public void testEqualsDifferentIndentation() {
+        this.checkNotEquals(
+            TextPrinting.with(
+                DIFFERENT_INDENTATION,
+                LINE_ENDING
+            )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentLineEnding() {
+        this.checkNotEquals(
+            TextPrinting.with(
+                INDENTATION,
+                DIFFERENT_LINE_ENDING
+            )
+        );
+    }
+
+    @Override
+    public TextPrinting createObject() {
+        return TextPrinting.with(
+            INDENTATION,
+            LINE_ENDING
+        );
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+            this.createObject(),
+            "indentation=\"  \" lineEnding=\"\\n\""
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TextPrinting> type() {
+        return TextPrinting.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
